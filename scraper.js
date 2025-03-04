415e7273931a50ebc96461935d6e17e7940d0c0b
Shuashuaa <jovijoshuatania027@gmail.com>
2025-03-04 19:48:03 +0800

    main functionality for scrapping

---
+++ scraper.js
@@ -0,0 +1,49 @@
+import puppeteer from "puppeteer";
+import fs from 'fs';
+
+const searchTerm = 'league of legends';
+
+const scrape = async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    const url = 'https://poptopiaph.com/collections/funko';
+
+    await page.goto(url);
+
+    await page.waitForSelector('input[name="q"]');
+    await page.waitForSelector('button.search-bar--submit');
+
+    await page.type('input[name="q"]', searchTerm);
+
+    await page.click('button.search-bar--submit');
+
+    await page.waitForNavigation();
+
+    await page.waitForSelector('.product-grid-item');
+
+    const popLinks = await page.evaluate(() => {
+        const popsElements = document.querySelectorAll('.product-grid-item');
+        return Array.from(popsElements).map((pop) => {
+            const popImage = pop.querySelector('.product-grid-image img')?.getAttribute('data-srcset')?.split(',')[0].split(' ')[0].replace(/^\/\//, '') || 'No Image';
+            const popTitle = pop.querySelector('p')?.textContent.trim() || 'No Title';
+            const popPrice = pop.querySelector('.product-item--price small')?.textContent.trim() || 'No Price';
+            const isSoldOut = pop.closest('.grid-item')?.classList.contains('sold-out') ? 'Sold Out' : 'Available';
+
+            return {
+                popImage, popTitle, popPrice, isSoldOut
+            };
+        });
+    });
+    if (fs.existsSync('links.json')) {
+        fs.unlinkSync('links.json');
+    }
+    fs.writeFileSync('links.json', JSON.stringify(popLinks, null, 2));
+    
+    console.log(popLinks);
+    console.log('Data saved to: links.json');
+
+    await browser.close();
+};
+
+scrape();
