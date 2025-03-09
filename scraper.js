26068d1b827df6f4a21b54c12966e0aadda59abe
Shuashuaa <jovijoshuatania027@gmail.com>
2025-03-09 22:29:45 +0800

    increased the timeout for the process and added a try-catch block

--- scraper.js
+++ scraper.js
@@ -12,41 +12,46 @@ const scrape = async () => {
 
     const url = process.env.SCRAPER_URL;
 
-    await page.goto(url);
+    try {
+        await page.goto(url, { timeout: 60000 })
 
-    await page.waitForSelector('input[name="q"]');
-    await page.waitForSelector('button.search-bar--submit');
+        await page.waitForSelector('input[name="q"]');
+        await page.waitForSelector('button.search-bar--submit');
 
-    await page.type('input[name="q"]', searchTerm);
+        await page.type('input[name="q"]', searchTerm);
 
-    await page.click('button.search-bar--submit');
+        await page.click('button.search-bar--submit');
 
-    await page.waitForNavigation();
+        await page.waitForNavigation({ timeout: 60000 });
 
-    await page.waitForSelector('.product-grid-item');
+        await page.waitForSelector('.product-grid-item');
 
-    const popLinks = await page.evaluate(() => {
-        const popsElements = document.querySelectorAll('.product-grid-item');
-        return Array.from(popsElements).map((pop) => {
-            const popImage = pop.querySelector('.product-grid-image img')?.getAttribute('data-srcset')?.split(',')[0].split(' ')[0].replace(/^\/\//, '') || 'No Image';
-            const popTitle = pop.querySelector('p')?.textContent.trim() || 'No Title';
-            const popPrice = pop.querySelector('.product-item--price small')?.textContent.trim() || 'No Price';
-            const isSoldOut = pop.closest('.grid-item')?.classList.contains('sold-out') ? 'Sold Out' : 'Available';
+        const popLinks = await page.evaluate(() => {
+            const popsElements = document.querySelectorAll('.product-grid-item');
+            return Array.from(popsElements).map((pop) => {
+                const popImage = pop.querySelector('.product-grid-image img')?.getAttribute('data-srcset')?.split(',')[0].split(' ')[0].replace(/^\/\//, '') || 'No Image';
+                const popTitle = pop.querySelector('p')?.textContent.trim() || 'No Title';
+                const popPrice = pop.querySelector('.product-item--price small')?.textContent.trim() || 'No Price';
+                const isSoldOut = pop.closest('.grid-item')?.classList.contains('sold-out') ? 'Sold Out' : 'Available';
 
-            return {
-                popImage, popTitle, popPrice, isSoldOut
-            };
+                return {
+                    popImage, popTitle, popPrice, isSoldOut
+                };
+            });
         });
-    });
-    if (fs.existsSync('links.json')) {
-        fs.unlinkSync('links.json');
-    }
-    fs.writeFileSync('links.json', JSON.stringify(popLinks, null, 2));
-    
-    console.log(popLinks);
-    console.log('Data saved to: links.json');
+        if (fs.existsSync('links.json')) {
+            fs.unlinkSync('links.json');
+        }
+        fs.writeFileSync('links.json', JSON.stringify(popLinks, null, 2));
+        
+        console.log(popLinks);
+        console.log('Data saved to: links.json');
 
-    await browser.close();
+    } catch (error) {
+        console.error('Scraping error:', error);
+    } finally {
+        await browser.close();
+    }
 };
 
 scrape();
