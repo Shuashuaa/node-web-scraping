7e1c6d178a766f61c21f4e34c49aa2be18ab20b4
Shuashuaa <jovijoshuatania027@gmail.com>
2025-03-10 22:36:54 +0800

    adding a start server for declared host as well as closing the port before running the scraper again

--- scraper.js
+++ scraper.js
@@ -1,11 +1,17 @@
-import puppeteer from "puppeteer";
+import puppeteer from 'puppeteer';
 import fs from 'fs';
 import dotenv from 'dotenv';
+import cron from 'node-cron';
+import open from 'open';
+import httpServer from 'http-server';
 
 dotenv.config();
 
 const searchTerm = process.env.SEARCH_TERM;
 
+let server; // Store the server reference
+
+// Function to perform the scraping task
 const scrape = async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
@@ -38,15 +44,47 @@ const scrape = async () => {
             };
         });
     });
+
     if (fs.existsSync('links.json')) {
         fs.unlinkSync('links.json');
     }
+
     fs.writeFileSync('links.json', JSON.stringify(popLinks, null, 2));
-    
+
     console.log(popLinks);
     console.log('Data saved to: links.json');
 
     await browser.close();
 };
 
-scrape();
+// Function to start the server
+const startServer = async () => {
+    try {
+        const port = '8081'; // Find an available port
+        const host = '192.168.1.5';
+
+        // Close the previous server if it exists
+        if (server) {
+            server.close(() => {
+                console.log('Previous server closed');
+            });
+        }
+
+        // Start a new server
+        server = httpServer.createServer({ root: '.' });
+
+        server.listen(port, host, () => {
+            console.log(`Server started on http://${host}:${port}`);
+            open(`http://${host}:${port}`).catch(console.error);
+        });
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+// cron job run evey 5 minutes
+cron.schedule('*/5 * * * *', async () => {
+    console.log('Running the scraper');
+    await scrape();
+    await startServer();
+});
